4c5dee767979fe68305c5279ae63e6f0b27f046e
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-14 18:23:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+//storing freaind names in array
+let freaindnames = ["Asad", "Aliyan", "Saad", "Muzaifer", "Sami"];
+console.log(freaindnames[0]);
+console.log(freaindnames[1]);
+console.log(freaindnames[2]);
+console.log(freaindnames[3]);
+console.log(freaindnames[4]);
